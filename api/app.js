6d8ca2ddfaff6dbf019d3c612acf58e24e13d765
NITHIN <nithics2004@gmail.com>
2025-07-25 21:54:54 +0530

    Integrate Cloudinary for file uploads

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
@@ -15,14 +15,10 @@ app.use(cors({ origin: 'https://book-management-app-iupm.vercel.app', credential
 // JSON parsing
 app.use(express.json());
 
-// Create uploads folder if missing
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use('/uploads', express.static('uploads'));
+
 // ✅ Routes
 const bookRoutes = require('./routes/books');
 
@@ -31,30 +27,3 @@ app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 
 module.exports = app;
-*/
-
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bookRoutes = require('./routes/book');
-const authRoutes = require('./routes/auth');
-require('dotenv').config();
-
-const app = express();
-app.use(cors({ origin: 'https://book-management-app-iupm.vercel.app', credentials: true }));
-// Important for image upload
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-app.use('/api/books', bookRoutes);
-app.use('/api/auth', authRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
