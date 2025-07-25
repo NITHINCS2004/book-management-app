9a335ff7ab05da274b1009649cad4ff9b16bae0f
NITHIN <nithics2004@gmail.com>
2025-07-25 18:28:54 +0530

    Fix: added missing path module for static file serving

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
-
+const path = require('path'); 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
