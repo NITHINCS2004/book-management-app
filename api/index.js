b0826f209ecc412e2ac6aa5b68ddf5efdf33f144
NITHIN <nithics2004@gmail.com>
2025-07-25 12:13:57 +0530

    Your update message here

--- api/index.js
+++ api/index.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => console.error(err));
